4883f5f1fb4e63cbb002ec41c5724d469e495899
noyee50 <noyee50@gmail.com>
2020-10-18 14:25:18 +0700

    update

--- server/server.js
+++ server/server.js
@@ -12,15 +12,32 @@ let io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on("connection", (socket) => {
-
   console.log("A new user connected");
+
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcom to Chat APP",
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New User Joined",
+    createDate: new Date().getTime(),
+  });
+
+  socket.on("createMessage", (message) => {
+    console.log("create message", message);
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createDate: new Date().getTime(),
+    });
+  });
 
   socket.on("disconnect", () => {
     console.log("User was Disconnected");
   });
-  
 });
-
 
 server.listen(port, () => {
   console.log(`Server run on port ${port}`);
