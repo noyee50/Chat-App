03b75017a2299ba478db69edbc23fb7b41fab41a
noyee50 <noyee50@gmail.com>
2020-11-01 10:54:39 +0700

    bbbbb

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
-const test = 'a';
+const test = 's';
 
 const { generateMessage, generateLocationMessage } = require("./utils/message");
 const publicPath = path.join(__dirname + "/../public");
